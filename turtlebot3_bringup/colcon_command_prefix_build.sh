3ea54625a2fdb92d7c22af42aeca13faa06ee28c
YKT412 <102668557+YKT412@users.noreply.github.com>
2024-01-28 05:56:44 +0530

    Add files via upload

---
+++ turtlebot3_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/smartlab/tb3/install/turtlebot3_description/share/turtlebot3_description/package.sh"
+. "/home/smartlab/tb3/install/turtlebot3_node/share/turtlebot3_node/package.sh"
